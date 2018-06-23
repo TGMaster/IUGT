16328406ea927f40c09dc2a00cbbf7752d33b4b4
TG <tien.grimes@gmail.com>
2018-06-23 14:51:49 +0700

    rcon

---
+++ src/main/java/Rcon/AuthenticationException.java
@@ -0,0 +1,9 @@
+package Rcon;
+
+public class AuthenticationException extends Exception {
+
+	public AuthenticationException(String message) {
+		super(message);
+	}
+	
+}
---
+++ src/main/java/Rcon/MalformedPacketException.java
@@ -0,0 +1,11 @@
+package Rcon;
+
+import java.io.IOException;
+
+public class MalformedPacketException extends IOException {
+	
+	public MalformedPacketException(String message) {
+		super(message);
+	}
+	
+}
